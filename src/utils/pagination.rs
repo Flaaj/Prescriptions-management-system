0beaee879de680aad92faf6fbee8670b3ea48370
Michał Wołek <m.wolek@nebucode.com>
2024-05-08 12:55:00 +0200

    add tests to paginaion params util

--- src/utils/pagination.rs
+++ src/utils/pagination.rs
@@ -7,7 +7,7 @@ pub enum PaginationError {
 pub fn get_pagination_params(
     page: Option<i64>,
     page_size: Option<i64>,
-) -> anyhow::Result<(i64, i64), PaginationError> {
+) -> anyhow::Result<(i64, i64)> {
     let page = page.unwrap_or(0);
     let page_size = page_size.unwrap_or(10);
     if page_size < 1 || page < 0 {
@@ -16,4 +16,21 @@ pub fn get_pagination_params(
     let offset = page * page_size;
 
     Ok((page_size, offset))
+}
+
+#[cfg(test)]
+mod unit_tests {
+    use super::*;
+
+    #[test]
+    fn test_get_pagination_params() {
+        assert_eq!(get_pagination_params(None, None).unwrap(), (10, 0));
+        assert_eq!(get_pagination_params(Some(0), Some(10)).unwrap(), (10, 0));
+        assert_eq!(get_pagination_params(Some(1), Some(10)).unwrap(), (10, 10));
+        assert_eq!(get_pagination_params(Some(1), Some(5)).unwrap(), (5, 5));
+        assert_eq!(get_pagination_params(Some(2), Some(5)).unwrap(), (5, 10));
+        assert_eq!(get_pagination_params(Some(13), Some(7)).unwrap(), (7, 91));
+        assert!(get_pagination_params(Some(0), Some(0)).is_err());
+        assert!(get_pagination_params(Some(-1), Some(10)).is_err());
+    }
 }
