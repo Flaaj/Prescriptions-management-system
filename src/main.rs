0beb9e1f4f7e3ffd3a86f06ab3052f46b1a56e09
Michał Wołek <m.wolek@nebucode.com>
2024-05-26 16:32:26 +0200

    uncommented code in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,68 @@
-// pub mod api;
-// mod create_tables;
+pub mod api;
+mod create_tables;
 pub mod domain;
-// pub mod infrastructure;
-// use std::sync::Arc;
+pub mod infrastructure;
+use std::sync::Arc;
 
-// use api::doctors_controller;
-// use create_tables::create_tables;
-// use domain::{
-//     doctors::service::DoctorsService, drugs::service::DrugsService,
-//     patients::service::PatientsService, pharmacists::service::PharmacistsService,
-// };
-// use infrastructure::postgres_repository_impl::{
-//     doctors::PostgresDoctorsRepository, drugs::PostgresDrugsRepository,
-//     patients::PostgresPatientsRepository, pharmacists::PostgresPharmacistsRepository,
-// };
+use api::doctors_controller;
+use create_tables::create_tables;
+use domain::{
+    doctors::service::DoctorsService, drugs::service::DrugsService,
+    patients::service::PatientsService, pharmacists::service::PharmacistsService,
+};
+use infrastructure::postgres_repository_impl::{
+    doctors::PostgresDoctorsRepository, drugs::PostgresDrugsRepository,
+    patients::PostgresPatientsRepository, pharmacists::PostgresPharmacistsRepository,
+};
 use rocket::{launch, Build, Rocket};
 use rocket_okapi::swagger_ui::{make_swagger_ui, SwaggerUIConfig};
-// use sqlx::{postgres::PgPoolOptions, PgPool};
+use sqlx::{postgres::PgPoolOptions, PgPool};
 
-// #[macro_use]
-// extern crate dotenv_codegen;
+#[macro_use]
+extern crate dotenv_codegen;
 
-// #[derive(Clone)]
-// pub struct Context {
-//     pub doctors_service: Arc<DoctorsService<PostgresDoctorsRepository>>,
-//     pub pharmacists_service: Arc<PharmacistsService<PostgresPharmacistsRepository>>,
-//     pub patients_service: Arc<PatientsService<PostgresPatientsRepository>>,
-//     pub drugs_service: Arc<DrugsService<PostgresDrugsRepository>>,
-// }
-// pub type Ctx = rocket::State<Context>;
+#[derive(Clone)]
+pub struct Context {
+    pub doctors_service: Arc<DoctorsService<PostgresDoctorsRepository>>,
+    pub pharmacists_service: Arc<PharmacistsService<PostgresPharmacistsRepository>>,
+    pub patients_service: Arc<PatientsService<PostgresPatientsRepository>>,
+    pub drugs_service: Arc<DrugsService<PostgresDrugsRepository>>,
+}
+pub type Ctx = rocket::State<Context>;
 
-// pub fn setup_context(pool: PgPool) -> Context {
-//     let doctors_repository = PostgresDoctorsRepository::new(pool.clone());
-//     let doctors_service = Arc::new(DoctorsService::new(doctors_repository));
-//     let pharmacists_rerpository = PostgresPharmacistsRepository::new(pool.clone());
-//     let pharmacists_service = Arc::new(PharmacistsService::new(pharmacists_rerpository));
-//     let patients_repository = PostgresPatientsRepository::new(pool.clone());
-//     let patients_service = Arc::new(PatientsService::new(patients_repository));
-//     let drugs_repository = PostgresDrugsRepository::new(pool.clone());
-//     let drugs_service = Arc::new(DrugsService::new(drugs_repository));
+pub fn setup_context(pool: PgPool) -> Context {
+    let doctors_repository = PostgresDoctorsRepository::new(pool.clone());
+    let doctors_service = Arc::new(DoctorsService::new(doctors_repository));
+    let pharmacists_rerpository = PostgresPharmacistsRepository::new(pool.clone());
+    let pharmacists_service = Arc::new(PharmacistsService::new(pharmacists_rerpository));
+    let patients_repository = PostgresPatientsRepository::new(pool.clone());
+    let patients_service = Arc::new(PatientsService::new(patients_repository));
+    let drugs_repository = PostgresDrugsRepository::new(pool.clone());
+    let drugs_service = Arc::new(DrugsService::new(drugs_repository));
 
-//     Context {
-//         doctors_service,
-//         pharmacists_service,
-//         patients_service,
-//         drugs_service,
-//     }
-// }
+    Context {
+        doctors_service,
+        pharmacists_service,
+        patients_service,
+        drugs_service,
+    }
+}
 
 #[launch]
 async fn rocket() -> Rocket<Build> {
-    // let pool = PgPoolOptions::new()
-    //     .max_connections(5)
-    //     .connect(dotenv!("DATABASE_URL"))
-    //     .await
-    //     .unwrap();
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(dotenv!("DATABASE_URL"))
+        .await
+        .unwrap();
 
-    // create_tables(&pool, true).await.unwrap();
+    create_tables(&pool, true).await.unwrap();
 
-    // let context = setup_context(pool);
+    let context = setup_context(pool);
 
     rocket::build()
-        // .manage(context)
-        // .mount("/", doctors_controller::get_routes())
+        .manage(context)
+        .mount("/", doctors_controller::get_routes())
         .mount(
             "/swagger-ui/",
             make_swagger_ui(&SwaggerUIConfig {
